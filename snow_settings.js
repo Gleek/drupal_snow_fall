7e7eeed06677bfe9914a608a4feebe04490bd281
Umar Ahmad <ahmad.umar2008@yahoo.in>
2014-03-17 19:36:39 +0530

    Improved rendering

--- snow_settings.js
+++ snow_settings.js
@@ -1,5 +1,3 @@
 $(document).ready(function(){
-    $(document).snowfall();
-    $('body').snowfall({flakeCount : 100, maxSpeed : 10});
-    window.setTimeout(function(){$('body').snowfall('clear');}, 5000);
-  });
+    $(document).snowfall({image :"flake.png", minSize: 50, maxSize:100});
+});
